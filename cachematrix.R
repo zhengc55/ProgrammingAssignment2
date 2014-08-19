7eea65d50a6ed33d640565080ccdd51aa714acbd
Chen Zheng <zhengc55@gmail.com>
2014-08-19 16:48:59 -0700

    push cachematrix up

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,33 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(data=NA,nrow=1,ncol=1,byrow=FALSE,dimnames=NULL) {
+    m <- NULL
+    x <- matrix(data,nrow,ncol,byrow,dimnames)
+    set <- function(z) {
+        x <<- z
+        m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inv) m <<- inv
+    getinv <- function() m
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()
+    if(!is.null(m)) {
+        message("getting cached data of matrix")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinv(m)
+    m
 }
